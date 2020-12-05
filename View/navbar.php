36856fe5d481f7f98ce9205d5fca6b915e710bcc
HolyDuckOfMeh <zhengjason68@gmail.com>
2020-12-04 21:51:28 -0500

    navbar mq

--- View/navbar.php
+++ View/navbar.php
@@ -1,7 +1,7 @@
     <!-- Navbar -->
     <div class='navbar-grid'>
-              <img style= 'grid-column: 2; grid-row: 1; height: 40px; width: 40px; float: right; margin: auto; margin-left: 85%;' src='./View/Public/Images/Corn.png'>
-              <h1 style='grid-column:3 ; grid-row: 1; text-align: center; color: #ffffff; font-weight: lighter; font-size: 30px; margin-top: 5px; margin-bottom: 5px; padding-top: 5x;'>Corn-Puter Computers</h1> 
+              <img class='navbar-corn' src='./View/Public/Images/Corn.png'>
+              <h1 class='navbar-title'>C<span>orn-</span>P<span>uter </span>C<span>omputers</span></h1> 
               <a href="http://localhost/FIT-Web-Course/ProjectCC/system-builder.php" class='btn-nav-system-builder btn btn-success' style ='background: rgba(0, 0, 0, 0.0); border-right: none;'>System Builder</a> 
               <a href="http://localhost/FIT-Web-Course/ProjectCC/featured-builds.php" class='btn-nav-featured-builds btn btn-success' style ='background: rgba(0, 0, 0, 0.0); border-right: none;'>Featured Builds</a> 
               <a href="http://localhost/FIT-Web-Course/ProjectCC/home.php" class='btn-nav-home btn btn-success' style ='background: rgba(0, 0, 0, 0.0); border-right: none;'>Home</a> 
