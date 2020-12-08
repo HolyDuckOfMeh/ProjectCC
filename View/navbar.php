8e12dc47c9393b0bed7ee8848d43423d5aabafa2
Jason Zheng <jzheng@training.local>
2020-12-08 08:58:55 -0500

    changed back to index

--- View/navbar.php
+++ View/navbar.php
@@ -4,7 +4,7 @@
               <h1 class='navbar-title'>C<span>orn-</span>P<span>uter </span>C<span>omputers</span></h1> 
               <a href="http://localhost/FIT-Web-Course/ProjectCC/system-builder.php" class='btn-nav-system-builder btn btn-success' style ='background: rgba(0, 0, 0, 0.0); border-right: none;'>System Builder</a> 
               <a href="http://localhost/FIT-Web-Course/ProjectCC/featured-builds.php" class='btn-nav-featured-builds btn btn-success' style ='background: rgba(0, 0, 0, 0.0); border-right: none;'>Featured Builds</a> 
-              <a href="http://localhost/FIT-Web-Course/ProjectCC/home.php" class='btn-nav-home btn btn-success' style ='background: rgba(0, 0, 0, 0.0); border-right: none;'>Home</a> 
+              <a href="http://localhost/FIT-Web-Course/ProjectCC/index.php" class='btn-nav-home btn btn-success' style ='background: rgba(0, 0, 0, 0.0); border-right: none;'>Home</a> 
               <!-- Will have to move the Home button to the left of the navbar later -->
               <div class='search-container'>
                 <input type="text" placeholder="Search.." name="search">
